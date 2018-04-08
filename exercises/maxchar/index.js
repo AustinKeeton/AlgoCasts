5b2168f060392c79295fe9bf57bba095ccc85cbf
Austin Keeton <austin@austins-mbp.attlocal.net>
2018-04-08 17:12:47 -0500

    Solve maxchar.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charMap = {};
+
+    for (let char of str) {
+        charMap[char] = ++charMap[char] || 1;
+    }
+
+    console.log(str, charMap);
+
+    let max = 0;
+    let maxChar = '';
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    console.log(str, '=> maxChar:', maxChar);
+    return maxChar;
+}
 
 module.exports = maxChar;
