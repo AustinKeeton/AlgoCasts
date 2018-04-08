af04bc0e85f6191e020b595939860a86f674d84f
Austin Keeton <austin@austins-mbp.attlocal.net>
2018-04-08 18:03:41 -0500

    Solve fizzbuzz.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,47 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// Straightforward version
+const fizzBuzz = n => {
+    for (let i = 1; i <= n; i++) {
+        if (i % (3*5) === 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+// Fancy version. >:)
+// const divides = (divisor, dividend) => dividend % divisor === 0;
+
+// const range = (start, end) => {
+//     if (start === end) { return [start] };
+//     return [start, ...range(start + 1, end)];
+// }
+
+// function fizzBuzz(n) {
+//     range(1, n).forEach(num => {
+//         if (divides(3*5, num)) {
+//             console.log('fizzbuzz');
+//         }
+//         else if (divides(5, num)) {
+//             console.log('buzz');
+//         }
+//         else if (divides(3, num)) {
+//             console.log('fizz');
+//         }
+//         else {
+//             console.log(num);
+//         }
+//     });
+// }
 
 module.exports = fizzBuzz;
